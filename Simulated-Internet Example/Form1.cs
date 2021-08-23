99986e3087da01b9dc72bfde6d3de8f047cd962a
bjames.12299 <bjames.12299@gmail.com>
2021-08-23 16:38:30 -0700

    Add project files.

---
+++ Simulated-Internet Example/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using SimulatedInternet;
+
+namespace Simulated_Internet_Example
+{
+    public partial class Form1 : Form
+    {
+        List<NetworkAppExample> examples = new List<NetworkAppExample>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LiveSetUp();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SimulationSetUp();
+        }
+
+        public void LiveSetUp()
+        {
+            NetworkAppExample example = new NetworkAppExample(null);
+            example.SetDestinationIP("127.0.0.1");
+            example.StartNetwork();
+        }
+
+        public void SimulationSetUp()
+        {
+            #region SETUP FOR SIMULATION
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            ///    THIS WILL AUTOMATICALLY BE DONE FOR THE CLIENTS IF THE HOST IP IS SET    ///
+            ///////////////////////////////////////////////////////////////////////////////////
+            
+            InternetManager manager = new InternetManager();
+
+            examples.Add(new NetworkAppExample(manager, NetworkAppExample.NetworkType.Simulated));
+            examples.Add(new NetworkAppExample(manager, NetworkAppExample.NetworkType.Simulated));
+
+            examples.First().SimulationID = "one";
+            examples.Last().SimulationID = "two";
+
+            manager.Details = new List<ConnectionDetail>();
+            manager.Identities = new List<DetailPackage>();
+
+            string IpOne = manager.RegisterIP();
+            string IpTwo = manager.RegisterIP();
+
+            manager.Identities.Add(new DetailPackage
+            {
+                IP = IpOne,
+                Member = examples.First(),
+                locationX = 0,
+                locationY = 0
+            });
+
+            manager.Identities.Add(new DetailPackage
+            {
+                IP = IpTwo,
+                Member = examples.Last(),
+                locationX = 50,
+                locationY = 50
+            });
+
+            manager.Details.Add(new ConnectionDetail
+            {
+                Member = examples.First(),
+                ConnectedMember = examples.Last(),
+                IP = IpOne,
+                DestIP = IpTwo,
+                Ping = manager.FakePing(manager.Identities.First(), manager.Identities.Last())
+            });
+
+            ConnectionDetail d = manager.LookUpConnection(IpOne, IpTwo);
+
+            examples.First().SetDestinationIP(IpTwo);
+            examples.Last().SetDestinationIP(IpOne);
+
+            manager.StartNetworkSimulation();            
+           
+            examples.First().StartNetwork();
+           
+            examples.Last().StartNetwork();
+
+            #endregion
+        }
+    }
+}
---
+++ Simulated-Internet Example/NetworkAppExample.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using SimulatedInternet;
+
+namespace Simulated_Internet_Example
+{
+    public class NetworkAppExample:NetworkMember
+    {
+        public enum NetworkType { Live = 22222, Simulated = 1337 };
+        public enum NetworkState { Listening, Hibernating}
+
+        public NetworkType Network = NetworkType.Simulated;
+        public NetworkState State = NetworkState.Hibernating;
+
+        public InternetManager SimulatedInternet { get; set; }
+
+        private Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        private string IpConnection = "";
+
+        public NetworkAppExample(InternetManager SimulatedInternet, NetworkType NetType = NetworkType.Live)
+        {
+            this.SimulatedInternet = SimulatedInternet;
+            Network = NetType;
+        }
+
+        public void SetDestinationIP(string IP)
+        {
+            IpConnection = IP;
+        }
+
+        public void StartNetwork()
+        {
+            StartListener();
+
+            Thread th = new Thread(NetworkBrain);
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        private void NetworkBrain()
+        {
+            int count = 0;
+
+            while(State == NetworkState.Listening)
+            {
+                Thread.Sleep(1000);
+
+                SendData(Encoding.UTF8.GetBytes("Testing Count: " + count++), IpConnection, this);
+            }
+        }
+
+        public override void DataListener(byte[] data, string SourceIP)
+        {
+            Console.WriteLine(SourceIP + " Sent: " + Encoding.UTF8.GetString(data));
+        }
+
+        public void SendData(byte[] data, string DestinationIP, NetworkMember Self)
+        {
+            if (Network == NetworkType.Simulated)
+            {
+                SimulatedInternet.SendBytesToIP(
+                                        this,
+                                        data,
+                                        DestinationIP,
+                                        (ushort)Network);
+            }
+            else
+            {
+                InternetSend(data, DestinationIP);
+            }
+        }
+
+        private void InternetSend(byte[] data, string ip)
+        {
+            IPEndPoint EndPoint = new IPEndPoint(IPAddress.Parse(ip), (int)Network);
+
+            sendSocket.SendTo(data, EndPoint);
+        }
+
+        private void StartListener()
+        {
+            State = NetworkState.Listening;
+
+            if (Network == NetworkType.Live)
+            {
+                new Thread(() =>
+                {
+                    UdpClient listener = new UdpClient((int)Network);
+                    IPEndPoint EndPoint = new IPEndPoint(IPAddress.Any, (int)Network);
+
+                    try
+                    {
+                        while (State == NetworkState.Listening)
+                        {
+                            byte[] bytes = listener.Receive(ref EndPoint);
+
+                            DataListener(bytes, EndPoint.ToString());
+                        }
+                    }
+                    catch (SocketException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    finally
+                    {
+                        listener.Close();
+                        State = NetworkState.Hibernating;
+                    }
+                }).Start();
+            }
+        }
+    }
+}
